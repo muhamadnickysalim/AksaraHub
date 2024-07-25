eda46053b02b997f8224884a1c08b758a42b34b0
Muhamad Nicky Salim <muhamadnickysalim@gmail.com>
2024-07-25 19:31:38 +0700

    [CHORES] Nicky: Fixing e2e testing enviroment

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,6 @@ import { getJestProjectsAsync } from '@nx/jest';
 export default async () => ({
   projects: await getJestProjectsAsync(),
   moduleNameMapper: {
-    '@testing-library/react': '<rootDir>/../../node_modules/@testing-library/react', // Add this line
+    '@testing-library/react': '<rootDir>/../../node_modules/@testing-library/react',
   },
 });
